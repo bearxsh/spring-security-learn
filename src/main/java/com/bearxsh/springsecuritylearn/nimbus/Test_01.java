c2505b974e18af9a8b12a64a075930ce4c53248a
bearxsh <bearxsh@163.com>
2022-10-13 17:49:05 +0800

    update

---
+++ src/main/java/com/bearxsh/springsecuritylearn/nimbus/Test_01.java
@@ -0,0 +1,46 @@
+package com.bearxsh.springsecuritylearn.nimbus;
+
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import io.jsonwebtoken.JwtException;
+
+import java.text.ParseException;
+
+public class Test_01 {
+
+    public static final String SECRET = "bellfsdafafwrqwerqwerqwerweqrwerwerwqrweqrwe";
+
+    public static void main(String[] args) throws JOSEException, ParseException {
+
+     /*   String token = generateToken();
+        verify(token);*/
+        verify("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb2UifQ.5jdU0nOYd05qTGPUAtrUdI7FACxWY1WYXZtmEZeAZHY");
+
+
+    }
+
+    public static String generateToken() throws JOSEException {
+        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.HS256)
+                .type(JOSEObjectType.JWT)
+                .build();
+        Payload payload = new Payload("{\"sub\":\"Joe\"}");
+        JWSObject jwsObject = new JWSObject(header, payload);
+        MACSigner signer = new MACSigner(SECRET);
+        jwsObject.sign(signer);
+        return jwsObject.serialize();
+    }
+
+    public static void verify(String token) throws ParseException, JOSEException {
+        JWSObject jwsObject = JWSObject.parse(token);
+        MACVerifier verifier = new MACVerifier(SECRET);
+        boolean result = jwsObject.verify(verifier);
+        if (!result) {
+            throw new JwtException("token 不合法");
+        }
+        // TODO 检查是否过期
+        String payload = jwsObject.getPayload().toString();
+        System.out.println(payload);
+
+    }
+}
--- src/main/java/com/bearxsh/springsecuritylearn/test/Test_01.java
+++ src/main/java/com/bearxsh/springsecuritylearn/test/Test_01.java
@@ -5,11 +5,19 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
 
 public class Test_01 {
     public static void main(String[] args) {
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("bellfsdafafwrqwerqwerqwerweqrwerwerwqrweqrwe");
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
         SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-        String jws = Jwts.builder().setSubject("Joe").signWith(secretKey).compact();
+        String jws = Jwts.builder().setHeaderParam("typ", "JWT").setSubject("Joe").signWith(signatureAlgorithm, signingKey).compact();
         System.out.println(jws);
 
     }
